fd4e8e724a4e04b2c0d877e5d9d1cf048a9faf75
aaravmenon130 <aaravmenon130@gmail.com>
2026-01-27 15:23:50 +0530

    Create teams power users.sql

---
+++ teams power users.sql
@@ -0,0 +1,13 @@
+-- https://datalemur.com/questions/teams-power-users
+
+SELECT 
+  sender_id,
+  COUNT(message_id)
+  FROM messages
+  WHERE 
+    EXTRACT(MONTH FROM sent_date) = 8 
+    AND 
+    EXTRACT(YEAR FROM sent_date) = 2022
+  GROUP BY sender_id
+  ORDER BY COUNT(message_id) DESC
+  LIMIT 2;
